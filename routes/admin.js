3d3036e7d99acf78c105d0f56c355a0a12335a02
Julia Aasmaa <my_e_10@hotmail.com>
2020-02-27 13:01:41 +0200

    v2

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const rootDirectory = require('../utilities/path');
+const products = [];
 
 //to use the code from this file in server.js
 const router = express.Router();
@@ -9,5 +10,13 @@ router.get('/add-product', function(req, res){
     
     res.sendFile(path.join(rootDirectory, 'views', 'add-product.html'));
 });
+
+router.post('/add-product', function(req, res){
+    console.log(req.body.title);
+    products.push({title: req.body.title});
+    res.redirect('/');
+});
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const adminData = require('./routes/admin');
 const path = require('path');
 
 const app = express();
+app.set('view engine', 'ejs');  
+app.set('views', 'views');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,14 +19,21 @@ app.use(userRoutes);
 
 
 app.use(function(req, res, next){
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {
+        pageTitle: "Oops! Page Not Found!",
+        pageNotFound: "No Luck. Try Again.",
+        myVariable: "Hello World!"
+    
+    });
+
+    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 
     //res.status(404).send("Not found");
 });
 
 
 
-app.listen(3000, function(){
+app.listen(5000, function(){
     console.log("Server is running on port 3000");
     //console.log("Path", path);
 });
